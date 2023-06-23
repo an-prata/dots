7a88f4df20c2991f729ef3ea3cd443fd91f87377
Evan Overman <evanrileyoverman@gmail.com>
2023-06-22 17:38:53 -0700

    fish alias

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    alias c="clear"
+    alias cp="cp -rv"
+    alias mv="mv -v"
+    alias rm="rm -rfv"
+    alias mkdir="mkdir -p"
 end
