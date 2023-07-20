71404c630ce56fd5b2460a667c6bf86224e7094b
Evan Overman <evanrileyoverman@gmail.com>
2023-07-19 19:45:32 -0700

    use dmenu cause every wayland launcher has something wrong with it

---
+++ hypr/scripts/dmenu.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+QT_QPA_PLATFORMTHEME=gtk2 j4-dmenu-desktop --no-generic --term='kitty' --dmenu="dmenu -i -b -p 'Launch: ' -nb '#17171c' -nf '#f2f2e9' -sb '#cca37a' -sf '#f2f2e9' -fn 'JetBrains Mono Nerd Font:pixelsize=14'"
+
