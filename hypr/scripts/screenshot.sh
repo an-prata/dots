39994ffcdae6d28fe125d3afaea27ea6d259b07b
Evan Overman <evanrileyoverman@gmail.com>
2023-07-20 18:16:04 -0700

    working screenshot

--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -3,9 +3,7 @@
 # Takes a screenshot and saves it to SAVE_DIR with the name FILE_NAME
 
 SAVE_DIR="$HOME/Pictures/Temporary Screenshots"
-FILE_NAME="Screenshot on $(date +"%B %d, %Y - %-I:%M:%S %p").png"
+FILE_NAME="Screenshot on $(date +"%A %B %d, %Y - %-I:%M:%S %p").png"
 
-maim -s | tee "$SAVE_DIR/$FILE_NAME" | xclip -selection clipboard -t image/png 
-notify-send "Took a screenshot" "$SAVE_DIR/$FILE_NAME" -a "scrot" -i "$SAVE_DIR/$FILE_NAME"
-#meh "$SAVE_DIR/$FILE_NAME" & disown
-
+grim -g "$(slurp)" "$SAVE_DIR/$FILE_NAME"
+notify-send "Took a screenshot" "$SAVE_DIR/$FILE_NAME" -a "grim" -i "$SAVE_DIR/$FILE_NAME"
