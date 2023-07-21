992b52ae3644a86293ece22baa309987bb391540
Evan Overman <evanrileyoverman@gmail.com>
2023-07-21 12:34:43 -0700

    screenshot notifs

--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -6,4 +6,7 @@ SAVE_DIR="$HOME/Pictures/Temporary Screenshots"
 FILE_NAME="Screenshot on $(date +"%A %B %d, %Y - %-I:%M:%S %p").png"
 
 grim -g "$(slurp)" "$SAVE_DIR/$FILE_NAME"
-notify-send "Took a screenshot" "$SAVE_DIR/$FILE_NAME" -a "grim" -i "$SAVE_DIR/$FILE_NAME"
+
+if [[ $? == "0" ]]; then
+  notify-send "Took a screenshot" "$SAVE_DIR/$FILE_NAME" -a "grim" -i "$SAVE_DIR/$FILE_NAME"
+fi
