844e5a50c5bbb3cbee1ab1edf2555fa942b9d41b
Evan Overman <evanrileyoverman@gmail.com>
2023-08-24 14:05:15 -0700

    improve scripts

--- waybar/scripts/media-art.sh
+++ waybar/scripts/media-art.sh
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 
+IMAGE_PATH="/tmp/waybar-media-art.png"
+MAGICK_OPTIONS="-gravity center -crop 4:1 -resize 128x32"
 playerStatus=$(playerctl -l --no-messages)
+
+# produce a new mask is one is missing
+if [ ! -f "$HOME/.config/waybar/mask.png" ]; then
+  convert -size 128x32 xc:black -fill white -draw "roundRectangle 0,0,128,32 16,16" "$HOME/.config/waybar/mask.png"
+fi
 
 if [ "$playerStatus" == "mpd" ]; then
   img=$(echo "$(playerctl metadata | grep artUrl | awk '{print $3}')" | cut -c 7-)
   if [ "$img" == "" ]; then
-    cp "$HOME/.config/waybar/default.png" "$HOME/.config/waybar/current.png"
+    cp "$HOME/.config/waybar/default.png" $IMAGE_PATH
   else
-    magick $img -gravity center -crop 4:1 "$HOME/.config/waybar/current.png"
+    magick $img $MAGICK_OPTIONS $IMAGE_PATH
   fi
 elif [ "${playerStatus:0:7}" == "firefox" ]; then
   path="$HOME/.mozilla/firefox/firefox-mpris/"
   image="$(ls $path)"
-  magick "$path$image" -gravity center -crop 4:1 "$HOME/.config/waybar/current.png"
+  magick "$path$image" $MAGICK_OPTIONS $IMAGE_PATH
 else
-  magick "$HOME/.config/waybar/default.png" -gravity center -crop 4:1 "$HOME/.config/waybar/current.png"
+  magick "$HOME/.config/waybar/default.png" $MAGICK_OPTIONS $IMAGE_PATH
 fi
 
-if [ ! -f "$HOME/.config/waybar/mask.png" ]; then
-  convert -size 128x32 xc:black -fill white -draw "roundRectangle 0,0,128,32 14,14" "$HOME/.config/waybar/mask.png"
-fi
-
-magick "$HOME/.config/waybar/current.png" -resize 128x32 "$HOME/.config/waybar/current.png"
-convert "$HOME/.config/waybar/current.png" "$HOME/.config/waybar/mask.png" -alpha Off -compose CopyOpacity -composite "$HOME/.config/waybar/current.png"
-echo "$HOME/.config/waybar/current.png"
+magick $IMAGE_PATH "$HOME/.config/waybar/mask.png" -alpha Off -compose CopyOpacity -composite $IMAGE_PATH
+echo $IMAGE_PATH
 echo "$(playerctl metadata --format '{{artist}}') - $(playerctl metadata --format '{{album}}')"
 
--- waybar/scripts/media.sh
+++ waybar/scripts/media.sh
@@ -2,9 +2,19 @@
 
 playerStatus=$(playerctl -l --no-messages)
 
-txt=" $(playerctl status --no-messages) 󰎉 $(playerctl metadata --format '{{artist}}') - $(playerctl metadata --format '{{title}}') "
+txt="$(playerctl status --no-messages) 󰎉 $(playerctl metadata --format '{{artist}}') - $(playerctl metadata --format '{{title}}')"
+tt=$txt
 
+# cut off strings longer than 64 chars
+if [ ${#txt} > 64 ]; then
+  txt="$(echo $txt | cut -c -61)..."
+fi
+
+# escape ampersands
 txt="${txt/&/&amp;}"
+tt="${tt/&/&amp;}"
 
 echo "$txt"
+echo "$tt"
+echo ""
 
