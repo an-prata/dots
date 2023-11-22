9438f22982b3a27cbdd9b8d6acc073117980e94e
Evan Overman <evanrileyoverman@gmail.com>
2023-11-22 00:33:18 -0800

    fix weird gtk stuff and new window rules

--- hypr/scripts/dmenu.sh
+++ hypr/scripts/dmenu.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
-QT_QPA_PLATFORMTHEME=gtk2 j4-dmenu-desktop --no-generic --term='kitty' --dmenu="dmenu-wl -i -b -p 'Launch: ' -nb '#17171c' -nf '#f2f2e9' -sb '#cca37a' -sf '#f2f2e9' -fn 'JetBrains Mono Nerd Font'"
+#QT_QPA_PLATFORMTHEME=gtk2 
+j4-dmenu-desktop --no-generic --term='kitty' --dmenu="dmenu-wl -i -b -p 'Launch: ' -nb '#17171c' -nf '#f2f2e9' -sb '#cca37a' -sf '#f2f2e9' -fn 'JetBrains Mono Nerd Font'"
 
