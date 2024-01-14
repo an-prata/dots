36dc4e5708a6491ef0dbe1c1dcf4ca35445f9d19
Evan Overman <evanrileyoverman@gmail.com>
2024-01-14 12:45:08 -0800

    open screenshot with action

--- hypr/scripts/screenshot.sh
+++ hypr/scripts/screenshot.sh
@@ -8,5 +8,9 @@ FILE_NAME="Screenshot on $(date +"%A %B %d, %Y - %-I:%M:%S %p").png"
 grim -g "$(slurp)" "$SAVE_DIR/$FILE_NAME"
 
 if [[ $? == "0" ]]; then
-  notify-send "Took a screenshot" "$SAVE_DIR/$FILE_NAME" -a "grim" -i "$SAVE_DIR/$FILE_NAME"
+  OPEN=$(notify-send "Took a screenshot" "$SAVE_DIR/$FILE_NAME" --action=OPEN=open -i "$SAVE_DIR/$FILE_NAME")
+
+  if [[ "$OPEN" == "OPEN" ]]; then 
+    viewnior "$SAVE_DIR/$FILE_NAME"
+  fi
 fi
