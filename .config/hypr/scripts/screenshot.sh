017bf2cb93791661f1ba60a90e24847a9e674d99
Evan Overman <evanrileyoverman@gmail.com>
2024-02-09 23:25:03 -0800

    screenshot actions on mako and wayland clipboard

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -8,9 +8,11 @@ FILE_NAME="Screenshot on $(date +"%A %B %d, %Y - %-I:%M:%S %p").png"
 grim -g "$(slurp)" "$SAVE_DIR/$FILE_NAME"
 
 if [[ $? == "0" ]]; then
-  OPEN=$(notify-send "Took a screenshot" "$SAVE_DIR/$FILE_NAME" --action=OPEN=open -i "$SAVE_DIR/$FILE_NAME")
+  wl-copy < "$SAVE_DIR/$FILE_NAME"
 
-  if [[ "$OPEN" == "OPEN" ]]; then 
+  OPEN=$(notify-send "Took a screenshot" "$SAVE_DIR/$FILE_NAME" --action 'default=default' -i "$SAVE_DIR/$FILE_NAME")
+
+  if [[ "$OPEN" == "default" ]]; then 
     viewnior "$SAVE_DIR/$FILE_NAME"
   fi
 fi
