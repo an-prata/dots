25ebe5feed2dbb5dab0233a7a1be4844d5295d23
Evan Overman <evanrileyoverman@gmail.com>
2024-05-06 15:25:46 -0700

    persistant window starting state

--- .config/hypr/scripts/window-start.fish
+++ .config/hypr/scripts/window-start.fish
@@ -1,23 +1,43 @@
 #!/usr/bin/env fish
 
-if test "$WINDOW_START" = tiling
-    if test "$argv[1]" = get
-        echo Tiling
-        exit
-    end
+switch $argv[1]
+    case get
+        switch $WINDOW_START
+            case tiling
+                echo Tiling
+                exit
+            case floating
+                echo Floating
+                exit
+            case '*'
+                echo Unknown
+                exit
+        end
 
-    hyprctl keyword windowrule 'float,title:.*'
-    set -U WINDOW_START floating
-    echo set floating
-else
-    if test "$argv[1]" = get
-        echo Floating
-        exit
-    end
+    case toggle
+        switch $WINDOW_START
+            case tiling
+                hyprctl keyword windowrule 'float,title:.*'
+                set -U WINDOW_START floating
+                echo set floating
+            case floating
+                hyprctl keyword windowrule 'tile,title:.*'
+                set -U WINDOW_START tiling
+                echo set tiling
 
-    hyprctl keyword windowrule 'tile,title:.*'
-    set -U WINDOW_START tiling
-    echo set tiling
+        end
+
+    case start
+        switch $WINDOW_START
+            case tiling
+                hyprctl keyword windowrule 'tile,title:.*'
+                set -U WINDOW_START tiling
+                echo set tiling
+            case floating
+                hyprctl keyword windowrule 'float,title:.*'
+                set -U WINDOW_START floating
+                echo set floating
+        end
 end
 
 pkill -RTMIN+3 waybar
