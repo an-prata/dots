5999824c6424203ce7ccabf79a1018c2015a1ed6
Evan Overman <evanrileyoverman@gmail.com>
2024-05-10 20:08:51 -0700

    put menu on bottom

--- .config/hypr/scripts/clipboard-history.fish
+++ .config/hypr/scripts/clipboard-history.fish
@@ -8,4 +8,4 @@ end
 
 set normal_background '#26262D'
 
-cliphist list | bemenu --nb $normal_background --ab $normal_background --hb $normal_background --fb $normal_background --tb $normal_background --nf '#f2f2e9' --af '#f2f2e9' --hf '#f2f2e9' --ff '#f2f2e9' --tf '#f2f2e9' --center --list 6 --margin 680 --fixed-height --fn 'JetBrainsMono 11' --wrap --prompt '' --ignorecase --prefix '->' --cw 1 --ch 16 --line-height 24 | cliphist decode | wl-copy
+cliphist list | bemenu --nb $normal_background --ab $normal_background --hb $normal_background --fb $normal_background --tb $normal_background --nf '#f2f2e9' --af '#f2f2e9' --hf '#f2f2e9' --ff '#f2f2e9' --tf '#f2f2e9' --bottom --fixed-height --fn 'JetBrainsMono 11' --wrap --prompt '' --ignorecase --prefix '->' --cw 1 --ch 16 --line-height 24 | cliphist decode | wl-copy
